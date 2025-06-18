2f324eb7eadbf339530fb436f5ff2bd407826560
Tejeshwer <tejeshwerkandimalla@gmail.com>
2025-06-18 18:43:15 -0400

    Initial scaffold

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,5 @@
-import { Slot } from 'expo-router';
-import 'expo-router/entry';
-import React from 'react';
-import { TailwindProvider } from 'tailwind-rn';
-
-
-import utilities from '../tailwind.json';
-
-export default function RootLayout(): JSX.Element {
-  return (
-    <TailwindProvider utilities={utilities as any}>
-      <Slot />
-    </TailwindProvider>
-  );
+import React from 'react'
+import { Slot } from 'expo-router'
+export default function RootLayout() {
+  return <Slot/>
 }
